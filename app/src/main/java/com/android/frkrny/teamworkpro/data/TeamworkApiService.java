a741c1128dacac716a3f28aad1b68c86e1d43888
frankrooney <frk.rny@gmail.com>
2017-08-10 19:45:43 +0100

    Finished with service for first api call.

--- app/src/main/java/com/android/frkrny/teamworkpro/data/TeamworkApiService.java
+++ app/src/main/java/com/android/frkrny/teamworkpro/data/TeamworkApiService.java
@@ -1,8 +1,12 @@
 package com.android.frkrny.teamworkpro.data;
 
 import com.android.frkrny.teamworkpro.data.model.ApiResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 /**
@@ -15,7 +19,18 @@ public interface TeamworkApiService {
     String BASE_URL = "http://www.eu.teamwork.com";
 
     @GET("/projects.json")
-    Call<ApiResponse> getProjects();
+    Call<ApiResponse> getActiveProjects();
 
+    /******** Helper class that sets up a new services *******/
+    class Creator {
 
+        public static TeamworkApiService newTeamworkService() {
+            Gson gson = new GsonBuilder().create();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(TeamworkApiService.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            return retrofit.create(TeamworkApiService.class);
+        }
+    }
 }
