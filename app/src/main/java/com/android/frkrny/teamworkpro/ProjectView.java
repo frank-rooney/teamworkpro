1215e6898279d9d1c81b0e051da0e6d4fb208a3e
frankrooney <frk.rny@gmail.com>
2017-08-12 18:35:24 +0100

    Creating a view for the project row and an adapter for the projects.

---
+++ app/src/main/java/com/android/frkrny/teamworkpro/ProjectView.java
@@ -0,0 +1,46 @@
+package com.android.frkrny.teamworkpro;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.frkrny.teamworkpro.data.model.Project;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by frankrooney on 12/08/2017.
+ * A {@link CardView} subclass that inflates a custom layout and handles binding data for a project.
+ */
+
+public class ProjectView extends CardView {
+
+    private ImageView projectLogo;
+    private TextView projectName;
+    private Project project;
+
+    public ProjectView(Context context) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.project_row, this, true);
+        projectName = (TextView) findViewById(R.id.project_name);
+        projectLogo = (ImageView) findViewById(R.id.project_logo);
+    }
+
+    public void bind(Project project) {
+        this.project = project;
+        displayTitle();
+        displayLogo();
+    }
+
+    private void displayTitle() {
+        projectName.setText(project.getName());
+    }
+
+    private void displayLogo() {
+        if(!TextUtils.isEmpty(project.getLogo())) {
+            Picasso.with(getContext()).load(project.getLogo()).fit().into(projectLogo);
+        }
+    }
+}
---
+++ app/src/main/java/com/android/frkrny/teamworkpro/ProjectsAdapter.java
@@ -0,0 +1,9 @@
+package com.android.frkrny.teamworkpro;
+
+/**
+ * Created by frankrooney on 12/08/2017.
+ * A {@link android.support.v7.widget.RecyclerView} adapter for displaying projects.
+ */
+
+public class ProjectsAdapter {
+}
